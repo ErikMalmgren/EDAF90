dd4cb4ce6f9a22313daa39d5d997903a291c8495
Whizma <isak@maatta.se>
2023-01-26 16:29:52 +0100

    labb 2 suger lite mindre

--- labb2/src/App.js
+++ labb2/src/App.js
@@ -2,27 +2,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import inventory from './inventory.ES6';
 import ComposeSalad from './ComposeSalad';
-import { useState } from 'react';
 
 function App()
 {
-  let extras = Object.keys(inventory).filter(name => inventory[name].extra);
-  const [base, setBase] = useState("Pasta");
   return (
-    <div className="container py-4">
+  <div className="container py-4">
     <header className="pb-3 mb-4 border-bottom">
       <span className="fs-4">Min egen salladsbar</span>
     </header>
 
-    {<ComposeSalad base={base} setBase={setBase} inventory={inventory}/>}
-
-
-    <div className="continer col-12">
-      <div className="row h-200 p-5 bg-light border rounded-3">
-        <h2>Välj innehållet i din sallad</h2>
-          {extras.map(name => <div key={name} className="col-4">{name}</div>)}
-      </div>
-    </div>
+    {<ComposeSalad inventory={inventory}/>}
 
     <footer className="pt-3 mt-4 text-muted border-top">
       EDAF90 - webbprogrammering
--- labb2/src/ComposeSalad.js
+++ labb2/src/ComposeSalad.js
@@ -1,17 +1,33 @@
 import { useState } from 'react';
 
 function ComposeSalad(props) {
-  let extras = Object.keys(props.inventory).filter(name => props.inventory[name].foundation);
+  let foundations = Object.keys(props.inventory).filter(name => props.inventory[name].foundation);
   const [foundation, setFoundation] = useState('Pasta'); 
+  
+  let proteins = Object.keys(props.inventory).filter(name => props.inventory[name].protein);
+  const [protein, setProtein] = useState('Kycklingfilé');
+  
+  let extras = Object.keys(props.inventory).filter(name => props.inventory[name].extra);
   const [extra, setExtra] = useState({Bacon: true, Fetaost: true}); 
 
   return (
     <div className="container col-12">
       <div className="row h-200 p-5 bg-light border rounded-3">
         <h2>Välj bas</h2>
-          <select value ={foundation} onChange={e => setFoundation(e.target.value)}>
-            {extras.map(name => <option key={name} value={name}> {name}</option>)}
+          <select value ={foundation} onChange = {e => setFoundation(...e.target.value)}>
+            {foundations.map(name => <option key={name}  value={name}>  {name}</option>)}
           </select>
+        <h2>Välj protein</h2>
+          <select value ={protein} onChange = {e => setProtein(...e.target.value)}>
+            {proteins.map(name => <option key={name}  value={name}>  {name}</option>)}
+          </select>
+        <h2>Välj tillbehör</h2>
+          {extras.map((item, index) => (
+             <div key={index} className="col-4 p-2 fs-7">
+             <input value={item} type="checkbox" />
+             <span> {item}</span>
+           </div>
+          ))}
       </div>
     </div>
   );
