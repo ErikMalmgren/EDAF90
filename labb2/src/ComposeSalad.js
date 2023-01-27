e2e01fbbc5137b58418edaa3891140a7e09088ea
Whizma <isak@maatta.se>
2023-01-27 11:34:44 +0100

    labb 2 pog

--- labb2/src/ComposeSalad.js
+++ labb2/src/ComposeSalad.js
@@ -8,26 +8,46 @@ function ComposeSalad(props) {
   const [protein, setProtein] = useState('Kycklingfilé');
   
   let extras = Object.keys(props.inventory).filter(name => props.inventory[name].extra);
-  const [extra, setExtra] = useState({Bacon: true, Fetaost: true}); 
+  const [extra, setExtra] = useState({Bacon: true, Fetaost: true});
+
+  let dressings = Object.keys(props.inventory).filter(name => props.inventory[name].dressing);
+  const [dressing, setDressing] = useState(); 
+
+  const handleCheckboxChange = event => {
+    const { name, checked } = event.target;
+    setExtra(oldState => {
+      const newState = { ...oldState };
+      newState[name] = checked;
+      return newState;
+    });
+  };
 
   return (
     <div className="container col-12">
       <div className="row h-200 p-5 bg-light border rounded-3">
-        <h2>Välj bas</h2>
+        <label>Välj bas
           <select value ={foundation} onChange = {e => setFoundation(e.target.value)}>
             {foundations.map(name => <option key={name}  value={name}>  {name}</option>)}
           </select>
-        <h2>Välj protein</h2>
+        </label>
+        <label>Välj protein
           <select value ={protein} onChange = {e => setProtein(e.target.value)}>
             {proteins.map(name => <option key={name}  value={name}>  {name}</option>)}
           </select>
-        <h2>Välj tillbehör</h2>
+        </label>
+        <label>Välj tillbehör
           {extras.map((item, index) => (
-             <div key={index} className="col-4 p-2 fs-7">
-             <input value={item} type="checkbox" />
+             <div key={index} className="col-3 p-1 fs-6">
+             <input value={item} type="checkbox" onChange= {handleCheckboxChange} name={item} checked={extra[item]} />
              <span> {item}</span>
            </div>
           ))}
+        </label>
+        <label>Välj dressing
+          <select value ={dressing} onChange = {e => setDressing(e.target.value)}>
+            {dressings.map(name => <option key={name}  value={name}>  {name}</option>)}
+          </select>
+        </label>
       </div>
     </div>
   );
---
+++ labb2/src/Salad.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const imported = require("./inventory.ES6.js");
+const { v4 : uuidv4} = require('uuid');
+
+function makeOptions(inv, prop) {
+  return Object.keys(inv)
+          .filter(name => inv[name][prop])
+          .map(name => '<option value="' + name + '"> ' + name + ", " +inv[name]['price'] + "kr" + '</option>')
+          .reduce((res, curr) => res + "\n" + curr, ""); //.join funkar här också
+}
+
+
+class Salad {
+  static instanceCounter = 0;
+  constructor(arg) {
+    const uuid = uuidv4();
+    this.uuid = uuid;
+    this.id = 'salad_' + Salad.instanceCounter++;
+    if (arg instanceof Salad) {
+      this.ingredients = {... arg.ingredients};
+      return;
+
+    } else if (typeof arg === "string") {
+      this.ingredients = JSON.parse(arg).ingredients;
+      //Kopia ska ha samma id
+      return;
+
+    }
+
+    this.ingredients = {};
+  }
+  add(name, property) {
+    this.ingredients[name] = property;
+    return this;
+   }
+  remove(name) {
+    delete this.ingredients[name];
+    return this;
+  }
+}
+
+Salad.prototype.getPrice = function () {
+  return Object.values(this.ingredients).reduce((previous, current) => previous + current.price*(current.amount || 1), 0);
+}
+//Beräkna amount i GourmentSalad classen
+
+Salad.prototype.count = function (property) {
+  return Object.values(this.ingredients).filter(prop => prop[property])
+                                        .reduce((previous, current) => previous + 1, 0); //reduce här kan bli .length
+}
+
+class GourmetSalad extends Salad{
+
+  add(name, property, amount = 1) {
+    let propCop = {...property};
+    propCop['amount'] = (amount + this.ingredients[name]?.amount ?? 0) ;
+    super.add(name, propCop);
+    return this;
+  }
+
+  //Copy på gourmentsalad kommer konflikta på amount
+
+}
