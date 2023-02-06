f0ab70169214d75987a3b94d57b753d71b455719
Whizma <isak@maatta.se>
2023-02-06 15:52:42 +0100

    pog

--- labb3/reflections.md
+++ labb3/reflections.md
@@ -1,3 +1,8 @@
 **Reflection question 1: How do you replace the application icon, favicon.ico?**
 
 * Upload another icon with same name, or change path 
+
+**Reflection Question 2: If you use nav-pills instead of nav-tabs the selected page is no longer highlighted in the menu, why? Hint: <NavLink> and the active css class.**
+
+* The pills do not use the .active css selector, while the tabs do
+
--- labb3/src/App.js
+++ labb3/src/App.js
@@ -4,10 +4,9 @@ import inventory from './inventory.ES6';
 import ComposeSalad from './ComposeSalad';
 import ViewOrder from './ViewOrder';
 import { useState } from 'react';
-import Salad from './Salad';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-
-function App() {
+function App(props) {
 
   const[salads, setSalads] = useState([]);
 
@@ -15,20 +14,47 @@ function App() {
     setSalads([...salads, salad]);
   }
   
-  return (
-  <div className="container py-4">
+  function Header() { return (
     <header className="pb-3 mb-4 border-bottom">
       <span className="fs-4">Min egen salladsbar</span>
     </header>
-    
-    {<ViewOrder shoppingCart={salads} />} 
-    {<ComposeSalad inventory={inventory} onSaladSubmit={handleSaladSubmit}/>}
+  )}
 
-    <footer className="pt-3 mt-4 text-muted border-top">
-      EDAF90 - webbprogrammering
-    </footer>
-  </div>
-  );
+  function Navbar(props) {
+    return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link className="nav-link" to="/compose-salad">
+          Komponera en sallad
+        </Link>
+      </li>
+      <li className = "nav-item">
+        <Link className="nav-link" to="/view-order">
+          Se kundvagn
+        </Link> 
+      </li>
+    </ul>);
   }
 
+  function PageContent(props) { 
+      return <div className="container py-4">
+                {<Header/>}
+                {<Navbar/>}
+                {<ViewOrder shoppingCart={salads} />} 
+                {<ComposeSalad inventory={inventory} onSaladSubmit={handleSaladSubmit}/>}
+                {<Footer/>} 
+              </div>
+      }
+            
+  function Footer(props) { 
+    return <footer className="pt-3 mt-4 text-muted border-top">
+              EDAF90 - webbprogrammering
+            </footer> 
+    }
+
+    return PageContent(props);
+} 
+
 export default App;
+
+
