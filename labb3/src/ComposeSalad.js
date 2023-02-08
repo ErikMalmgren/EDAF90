791d21ead0a87c81a92fd1cad11dff3e34e183bf
Whizma <isak@maatta.se>
2023-02-08 13:58:47 +0100

    this was mos def it chief - W

--- labb3/src/ComposeSalad.js
+++ labb3/src/ComposeSalad.js
@@ -56,62 +56,61 @@ function ComposeSalad(props) {
   return (
     
     <div className="container col-12">
-    <form onSubmit={handleSubmit} noValidate>
-    <div className="row h-200 p-5 bg-light border rounded-3">
+      <form onSubmit={handleSubmit} noValidate>
+        <div className="row h-200 p-5 bg-light border rounded-3">
+          
+          <h2>Välj bas</h2>
+            <div>
+              <select required name={foundation} onChange={e => {setFoundation(e.target.value); e.target.parentElement.classList.add("was-validated");}}>
+                <option hidden value = "">Välj en bas!</option>
+                {foundations.map(name => <option key={name} value={name}>{name}</option>)}
+              </select>
+              <div className="valid-feedback"> Bra val!</div> 
+              <div className="invalid-feedback"> Du måste välja en salladsbas!</div> 
+            </div>
 
-      <h2>Välj bas</h2>
-        <div>
-        <select required name={foundation} onChange={e => {setFoundation(e.target.value); e.target.parentElement.classList.add("was-validated");}}>
-          <option hidden value = "">Välj en bas!</option>
-            {foundations.map(name => <option key={name} value={name}>{name}</option>)}
-          </select>
-          <div className="valid-feedback"> Bra val!</div> 
-          <div className="invalid-feedback"> Du måste välja en salladsbas!</div> 
+          <h2>Välj protein</h2>
+            <div> 
+              <select required name={protein} onChange={e => {setProtein(e.target.value); e.target.parentElement.classList.add("was-validated");}}>
+                <option hidden value = "">Välj ett protein!</option>
+                {proteins.map(name => <option key={name} value={name}>{name}</option>)}
+              </select>
+              <div className="valid-feedback"> Bra val!</div> 
+              <div className="invalid-feedback"> Du måste välja ett protein!</div> 
+            </div>
+
+          <h2>Välj tillbehör</h2>
+            {extras.map((item, index) => (
+              <div key={index} className="col-3 p-2 fs-6">
+                <input value={item} type="checkbox" onChange={handleCheckboxChange} name={item} checked={!!extra[item]} style={{ marginRight: "5px" }} />
+                  <Link 
+                    to={`/view-ingredient/${item}`}
+                    style={{textDecoration: "none"}}>
+                    {item}
+                  </Link>
+              </div>
+            ))}
+
+          <h2>Välj dressing</h2>
+
+            <div>
+              <select required name={dressing} onChange= {e => {setDressing(e.target.value); e.target.parentElement.classList.add("was-validated");}}>
+                <option hidden value = "">Välj en dressing!</option>
+                {dressings.map(name => <option key={name} value={name}>{name}</option>)}
+              </select>
+              <div className="valid-feedback"> Bra val!</div> 
+              <div className="invalid-feedback"> Du måste välja en dressing!</div> 
+            </div>
+
+            <div class = "btn-group">
+              <button type="submit" className="btn btn-primary border rounded-3">Beställ</button> 
+              <button type="reset" className="btn btn-primary border rounded-3" onClick={resetSalad}>Återställ</button>
+            </div>
+
         </div>
-
-      <h2>Välj protein</h2>
-        <div> 
-          <select required name={protein} onChange={e => {setProtein(e.target.value); e.target.parentElement.classList.add("was-validated");}}>
-          <option hidden value = "">Välj ett protein!</option>
-            {proteins.map(name => <option key={name} value={name}>{name}</option>)}
-          </select>
-          <div className="valid-feedback"> Bra val!</div> 
-          <div className="invalid-feedback"> Du måste välja ett protein!</div> 
-        </div>
-
-      <h2>Välj tillbehör</h2>
-
-        {extras.map((item, index) => (
-           <div key={index} className="col-3 p-2 fs-6">
-           <input value={item} type="checkbox" onChange={handleCheckboxChange} name={item} checked={!!extra[item]} style={{ marginRight: "5px" }} />
-           <Link 
-            to={`/view-ingredient/${item}`}
-            style={{textDecoration: "none"}}>
-            {item}
-            </Link>
-         </div>
-        ))}
-
-      <h2>Välj dressing</h2>
-
-        <div>
-          <select required name={dressing} onChange= {e => {setDressing(e.target.value); e.target.parentElement.classList.add("was-validated");}}>
-          <option hidden value = "">Välj en dressing!</option>
-            {dressings.map(name => <option key={name} value={name}>{name}</option>)}
-          </select>
-          <div className="valid-feedback"> Bra val!</div> 
-          <div className="invalid-feedback"> Du måste välja en dressing!</div> 
-        </div>
-
-        <div class = "btn-group">
-          <button type="submit" className="btn btn-primary border rounded-3">Beställ</button> 
-          <button type="reset" className="btn btn-primary border rounded-3" onClick={resetSalad}>Återställ</button>
-        </div>
-
+      </form>
     </div>
-  </form>
-</div>
-
   );
 }
+
 export default ComposeSalad;
